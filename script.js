b32cd099e1b44703b75418a48a5a157559f50a3e
AdrianZdankowski <104324440+AdrianZdankowski@users.noreply.github.com>
2022-06-01 18:07:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,70 +1,101 @@
 const turn = document.getElementById("turn")
 const resetbtn = document.getElementById("resetbtn")
-let round = "X"
-const board = [[null, null, null], [null, null, null], [null, null, null]]
-let winner = null
-turn.innerHTML = round + "'s turn"
+const cells = document.querySelectorAll(".cell")
+const gamemodes = document.querySelectorAll(".gamemode")
+const selectGamemodeBtn = document.getElementById("selectGamemodeBtn")
 
-function exists(arr, search) {
-    return arr.some(row => row.includes(search));
+let selectedGamemode = null
+let round = "X"
+let winner = false
+
+const board = Array.from({ length: 3 }).map(_ => [null, null, null])
+turn.textContent = round + "'s turn"
+
+const exists = (arr, search) => arr.some(row => row.includes(search))
+
+function openNav() {
+    document.getElementById("select_gamemode").style.height = "100%";
 }
 
-document.querySelectorAll('.cell').forEach(item => {
-    item.addEventListener('click', event => {
-        if (winner == null) {
-        if (round == "X" && (board[item.id[0]][item.id[1]]) === null) {
-            item.innerHTML = round
+function closeNav() {
+    document.getElementById("select_gamemode").style.height = "0%";
+    gamemodes.forEach(mode => mode.addEventListener("click", gamemodePick, { once: true }))
+}
+
+const gamemodePick = event => {
+    const mode = event.target
+    selectedGamemode = mode.id
+    closeNav()
+}
+
+const handleClick = event => {
+    const item = event.target
+    if (selectedGamemode == "pvp" && selectGamemodeBtn != null)
+        if (!winner) {
+            item.textContent = round
             board[item.id[0]][item.id[1]] = round
-            round = "O"
-            turn.innerHTML = round + "'s turn"
-        }
+            round = round == "X" ? "O" : "X"
+            turn.textContent = round + "'s turn"
 
-        if (round == "O" && (board[item.id[0]][item.id[1]]) === null) {
-            item.innerHTML = round
-            board[item.id[0]][item.id[1]] = round
-            round = "X"
-            turn.innerHTML = round + "'s turn"
-        }
-
-        //Horizontal lines
-        for (let i = 0; i < 3; i++) {
-            if (board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] != null) {
-                turn.innerHTML = board[i][0] + " wins!"
-                winner = 1
-                break
+            //Horizontal lines
+            for (let i = 0; i < 3; i++) {
+                if (
+                    board[i][0] == board[i][1] &&
+                    board[i][0] == board[i][2] &&
+                    board[i][0] != null
+                ) {
+                    turn.textContent = board[i][0] + " wins!"
+                    winner = true
+                    break
+                }
             }
-        }
 
-        //Vertical lines
-        for (let i = 0; i < 3; i++) {
-            if (board[0][i] == board[1][i] && board[0][i] == board[2][i] && board[0][i] != null) {
-                turn.innerHTML = board[0][i] + " wins!"
-                winner = 1
-                break
+            //Vertical lines
+            for (let i = 0; i < 3; i++) {
+                if (
+                    board[0][i] == board[1][i] &&
+                    board[0][i] == board[2][i] &&
+                    board[0][i] != null
+                ) {
+                    turn.textContent = board[0][i] + " wins!"
+                    winner = true
+                    break
+                }
             }
-        }
 
-        //Diagonal lines
-        if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != null) {
-            turn.innerHTML = board[0][0] + " wins!"
-                winner = 1
-                
-        }
-
-        if (board[2][0] == board[1][1] && board[2][0] == board[0][2] && board[2][0] != null) {
-            turn.innerHTML = board[2][0] + " wins!"
-                winner = 1
-             
+            //Diagonal lines
+            if (
+                board[0][0] == board[1][1] &&
+                board[0][0] == board[2][2] &&
+                board[0][0] != null
+            ) {
+                turn.textContent = board[0][0] + " wins!"
+                winner = true
             }
-            
+
+            if (
+                board[2][0] == board[1][1] &&
+                board[2][0] == board[0][2] &&
+                board[2][0] != null
+            ) {
+                turn.textContent = board[2][0] + " wins!"
+                winner = true
+            }
+
             if (!exists(board, null) && winner == null) {
-                turn.innerHTML = "Tie!"
-                winner = 1
+                turn.textContent = "Tie!"
+                winner = true
             }
         }
-        
-    })
-})
+}
+
+gamemodes.forEach(mode => mode.addEventListener("click", gamemodePick, { once: true }))
+
+selectGamemodeBtn.addEventListener("click", openNav)
+
+cells.forEach(cell =>
+    cell.addEventListener("click", handleClick, { once: true })
+)
 
 resetbtn.addEventListener("click", () => {
     winner = null
@@ -73,12 +104,12 @@ resetbtn.addEventListener("click", () => {
             board[i][j] = null
         }
     }
-    
-    document.querySelectorAll('.cell').forEach(item => {
-        item.innerHTML = ''
+
+    document.querySelectorAll(".cell").forEach(cell => {
+        cell.textContent = ""
+        cell.addEventListener("click", handleClick, { once: true })
     })
 
     round = "X"
-    turn.innerHTML = round + "'s turn"
-    
+    turn.textContent = round + "'s turn"
 })
